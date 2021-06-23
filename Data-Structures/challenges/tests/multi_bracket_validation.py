4897962dd9f2e1376c3254bfbdec1eb9d123e115
amarh-ayman <amarhbishtawi@gmail.com>
2021-06-23 10:12:36 +0300

    edit

--- Data-Structures/challenges/tests/multi_bracket_validation.py
+++ Data-Structures/challenges/tests/multi_bracket_validation.py
@@ -1,18 +1,17 @@
 from tests.test_queue_with_stacks import data
-from multi_bracket_validation.multi_bracket_validation import multi_bracket_validation
+from multi_bracket_validation.multi_bracket_validation import check
 
 import pytest
 
 def test_multi_bracket_validation_01():
-  data=multi_bracket_validation()
-  assert data.check('{}{Code}[Fellows](())')==True
-  assert data.check('{}(){}')==True
-  assert data.check('()[[Extra Characters]]')==True
-  assert data.check('{')==False
-  assert data.check('{}')==True
-  assert data.check('{)')==False
-  assert data.check('{)()')==False
+  assert check('{}{Code}[Fellows](())')==True
+  assert check('{}(){}')==True
+  assert check('()[[Extra Characters]]')==True
+  assert check('{')==False
+  assert check('{}')==True
+  assert check('{)')==False
+  assert check('{)()')==False
   
-  assert data.check('[({}]')==False
-  assert data.check('(](')==False
-  assert data.check('{(})')==False
+  assert check('[({}]')==False
+  assert check('(](')==False
+  assert check('{(})')==False
