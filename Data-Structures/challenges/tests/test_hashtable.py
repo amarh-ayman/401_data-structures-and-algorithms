1d233ab1424b515a381338571be8bddf93856a73
amarh-ayman <amarhbishtawi@gmail.com>
2021-07-26 00:10:37 +0300

    add to test

--- Data-Structures/challenges/tests/test_hashtable.py
+++ Data-Structures/challenges/tests/test_hashtable.py
@@ -3,25 +3,40 @@ from hashTable.hashtable import Hashtable
 def test ():
   pass
 
-def test_addingToHashTable():
-  hashtable = Hashtable()
-  hashtable.add('ahmad', 25)
-  hashtable.add('hamad', 29)
-  assert len(set(hashtable.map))==2
+def test_hash_create():
+    ht = Hashtable(10)
+    assert ht.map == [None, None, None, None, None, None, None, None, None, None]
 
-def test_getValueAdded():
-  hashtable = Hashtable()
-  hashtable.add('ahmad', 25)
-  hashtable.add('hamad', 29)
-  assert hashtable.get('ahmad')==25
+def test_hash_add():
+    ht = Hashtable(1024)
+    ht.add('name', 'amarh')
+    keyHashed =ht.hash('name')
+    assert ht.map[keyHashed].head.value == ('name', 'amarh')
+    
 
-def test_contain():
-  hashtable = Hashtable()
-  hashtable.add('ahmad', 25)
-  hashtable.add('hamad', 29)
-  assert hashtable.contains('ahmad')==True
-  assert hashtable.contains('am')=='Null'
-  
-def test_hash():
-  hashtable = Hashtable()
-  assert hashtable.hash('ab')==97  
+def test_hash_get():
+    ht = Hashtable(1024)
+    ht.add('name', 'amarh')
+    assert ht.get('name') == 'amarh'
+
+def test_hash_get_not():
+    ht = Hashtable(1024)
+    assert ht.get('name') == None
+
+def test_hash_add_collision():
+    ht = Hashtable(1024)
+    ht.add('name', 'amarh')
+    ht.add('mane', 'not amarh')
+    assert ht.get('name') == 'amarh'
+    assert ht.get('mane') == 'not amarh'
+
+def test_hash_hash():
+    ht = Hashtable(1024)
+    key = 'name'
+    assert ht.hash(key) == 491
+
+def test_hash_contains():
+    ht = Hashtable(1024)
+    ht.add('name', 'amarh')
+    assert ht.contains('name') == True
+    assert ht.contains('phone') == False
