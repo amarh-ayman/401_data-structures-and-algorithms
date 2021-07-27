295e40fe74e4eb07cec0c61acb551786b143fc37
amarh-ayman <amarhbishtawi@gmail.com>
2021-07-27 23:50:16 +0300

    done

--- Data-Structures/challenges/Algorithm/readmeFiles/hashmap_repeated_word.md
+++ Data-Structures/challenges/Algorithm/readmeFiles/hashmap_repeated_word.md
@@ -1,16 +1,16 @@
 # Challenge Summary
 
-Write a function called repeated word that finds the first word to occur more than once in a string
+Write a function called tree intersection which find the common values in both binary trees and return array
 
 ## Whiteboard Process
 
-![hashmap_repeated_word](../../../../imgs/hashmap.png)
+![hashmap_repeated_word](../../../../imgs/hashmap-tree-intersection.png)
 
 ## Approach & Efficiency
 
-time O(n)
-space O(3)
+time O(n\*n)
+space O(1)
 
 ## Solution
 
-[hashmap_repeated_word](https://github.com/amarh-ayman/401_data-structures-and-algorithms/tree/main/Data-Structures/challenges/Algorithm/hashmap_repeated_word/hashmap_repeated_word.py)
+[hashmap_repeated_word](https://github.com/amarh-ayman/401_data-structures-and-algorithms/tree/main/Data-Structures/challenges/Algorithm/hashmap_tree_intersection/hashmap_tree_intersection.py)
---
+++ Data-Structures/challenges/tests/test_hashmap_tree_intersection.py
@@ -0,0 +1,53 @@
+from Algorithm.hashmap_tree_intersection.hashmap_tree_intersection  import treeIntersection
+
+from Algorithm.hashmap_tree_intersection.BinarySearchTree  import *
+
+import pytest
+
+def test_bothFull(data1,data2) :
+  assert treeIntersection(data1,data2)==[8,42,16]
+    
+
+def test_Oneempty(data1):
+  tree=BinarySearchTree()
+  assert treeIntersection(data1,tree)==[]
+
+def test_bothempty():
+  tree=BinarySearchTree()
+  tree2=BinarySearchTree()
+  assert treeIntersection(tree,tree2)==[]
+    
+def test_notContain():
+  tree=BinarySearchTree()
+  tree.Add(1)
+  tree.Add(12)
+  tree.Add(31)
+  tree2=BinarySearchTree()
+  tree.Add(100)
+  tree.Add(50)
+  tree.Add(88)
+
+  assert treeIntersection(tree,tree2)==[]
+
+
+@pytest.fixture
+def data1():
+    tree1 = BinarySearchTree()
+    tree1.Add(23)
+    tree1.Add(8)
+    tree1.Add(42)
+    tree1.Add(4)
+    tree1.Add(16)
+    tree1.Add(27)
+    return tree1
+
+@pytest.fixture
+def data2():  
+    tree2 = BinarySearchTree()
+    tree2.Add(100)
+    tree2.Add(8)
+    tree2.Add(42)
+    tree2.Add(7)
+    tree2.Add(16)
+    tree2.Add(9)   
+    return tree2 
